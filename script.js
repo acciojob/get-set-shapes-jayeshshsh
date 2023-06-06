cd1ded3f41844fb8b3ae5f474d8477c830550e4c
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-06-06 19:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 //complete this code
 class Rectangle {}
+constructor(width,height){
+	this.width = width;
+	this.height = height;
+}
+get width(){
+	return this.width;
+}
 
+get height(){
+	return this.height;
+}
+getArea(){
+	return this.width*this.height;
+}
 class Square extends Animal {}
 
 // Do not change the code below this line
